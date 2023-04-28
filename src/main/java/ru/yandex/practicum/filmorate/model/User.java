85b113d526f0890f9650bb5f950e862ae8772477
Евгений <89537796626@mail.ru>
2023-04-28 17:35:44 +0700

    test2

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode
+public class User {
+    private int id; //целочисленный идентификатор
+    private String email; //электронная почта
+    private String login; //логин пользователя
+    private String name; //имя для отображения
+    private LocalDate birthday; //дата рождения
+
+    public User(int id, String email, String login, String name, LocalDate birthday) {
+
+        this.id = id;
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public int setId(int id) {
+        this.id = id;
+        return id;
+    }
+}
