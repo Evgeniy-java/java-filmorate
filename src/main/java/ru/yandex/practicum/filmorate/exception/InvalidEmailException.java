aa73f935bd5754657aef63e6817da85edea07127
Evgeniy-java <89537796626@mail.ru>
2023-04-29 14:18:53 +0700

    Checkstyle

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InvalidEmailException.java
@@ -0,0 +1,5 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class InvalidEmailException extends Throwable{
+    public InvalidEmailException(String s){super(s);}
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -0,0 +1,5 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class ValidationException extends Throwable{
+    public ValidationException(String s){super(s);}
+}
