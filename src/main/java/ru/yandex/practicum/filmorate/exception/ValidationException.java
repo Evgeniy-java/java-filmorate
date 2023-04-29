a6d9e8d96b35b27c9c85487543c22deefbb58e08
Evgeniy-java <89537796626@mail.ru>
2023-04-29 13:54:55 +0700

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class ValidationException extends Throwable {
-    public ValidationException(String v) {
-        super(v);
+public class ValidationException extends Throwable{
+    public ValidationException(String s){
+        super(s);
     }
 }
