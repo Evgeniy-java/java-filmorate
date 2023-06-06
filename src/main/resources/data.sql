d44312682691387f5154c79e1066fab358778b94
Евгений <89537796626@mail.ru>
2023-06-06 11:34:59 +0700

    SQL-запросы

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO MPAA (NAME) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17'), ('M'), ('X');
+
+INSERT INTO GENRES (GENRE_NAME) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик'), ('Ужасы');
