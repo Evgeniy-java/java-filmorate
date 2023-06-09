e8c97065b7c7649920fb716099f8584730201b89
Евгений <89537796626@mail.ru>
2023-06-09 16:30:55 +0700

    checkStyle

--- src/main/java/ru/yandex/practicum/filmorate/impl/GenresDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/impl/GenresDaoImpl.java
@@ -50,6 +50,7 @@ public class GenresDaoImpl implements GenresDao {
     public boolean mpaExists(long id) {
         String sql = "select count (genre_name) from genres where genre_id = ?";
         Long count = jdbcTemplate.queryForObject(sql, Long.class, id);
+        
         return count != 0;
     }
 
