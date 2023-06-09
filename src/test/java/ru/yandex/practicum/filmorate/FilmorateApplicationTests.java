bfb509693e82ce279d94aed750d8cc4f5b6b22cd
Евгений <89537796626@mail.ru>
2023-06-09 17:35:35 +0700

    testFindUserById

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,18 +1,12 @@
 package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.model.User;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class FilmoRateApplicationTests {
+@SpringBootTest
+class FilmorateApplicationTests {
 
     @Test
-    public void testFindUserById() {
-        User user = new User(1, "email", "login", "name", LocalDate.now());
-
-        assertEquals(1, user.getId());
+    void contextLoads() {
     }
 }
