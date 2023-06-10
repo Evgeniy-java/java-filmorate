cd706cfa2f6c970f7cbb74bad1a5b58df3fffe75
Evgeniy-java <89537796626@mail.ru>
2023-06-10 19:37:12 +0700

    test pos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,3 @@
 INSERT INTO MPA (NAME) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
-DELETE FROM MPA;
 
 INSERT INTO GENRES (GENRE_NAME) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
-DELETE FROM GENRES;
