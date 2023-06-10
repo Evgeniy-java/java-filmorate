d5fac9c8016a283ef921b7b343d30045eea4750c
Evgeniy-java <89537796626@mail.ru>
2023-06-11 04:28:01 +0700

    добавил test FriendDao

---
+++ src/test/java/ru/yandex/practicum/filmorate/daoTest/FriendDaoTest.java
@@ -0,0 +1,41 @@
+package ru.yandex.practicum.filmorate.daoTest;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import ru.yandex.practicum.filmorate.dao.FriendsDao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Sql({"/data-test.sql"})
+public class FriendDaoTest {
+    private final FriendsDao friendsDao;
+
+    @Test
+    void addToFriends() {
+        friendsDao.addToFriends(2, 1);
+        assertThat(friendsDao.getUserFriendsById(1)).hasSize(1);
+    }
+
+    @Test
+    void deleteFromFriends() {
+        friendsDao.deleteFriend(1, 2);
+        assertThat(friendsDao.getUserFriendsById(1)).hasSize(0);
+    }
+
+    @Test
+    void getFriendsIds() {
+        assertThat(friendsDao.getUserFriendsById(1)).hasSize(1);
+    }
+
+    @Test
+    void getCommonFriendsIds() {
+        assertThat(friendsDao.getCommonFriends(2, 1)).hasSize(0);
+    }
+}
